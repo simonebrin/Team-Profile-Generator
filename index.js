e10601cac3e4ef2d715f37b4fe3f7fe7bae3a3bd
Simone Brin <simonebrin@gmail.com>
2022-08-28 10:13:11 -0400

    added other employee questions and functions

--- index.js
+++ index.js
@@ -29,10 +29,56 @@ function promptManager() {
     inquirer.prompt(managerQuestions).then(answers => {
         console.log(answers)
     })
-    promptMenu();
+    // promptMenu();
 }
 
-function promptMenu() {}
+const otherEmployeesQuestions = [
+    {
+        type: "list",
+        name: "otherEmployees",
+        message: "Would you like to add the following other employees?",
+        choices: ["Engineer", "Intern"],
+    }
+]
+
+const engineerQuestions = [
+    {
+        type: "input",
+        name: "engineerName",
+        message: "What is the engineer's name?",
+    },
+    {
+        type: "input",
+        name: "engineerId",
+        message: "What is your employee ID?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
+    {
+        type: "input",
+        name: "gitUn",
+        message: "What is your GitHub username?"
+    }
+]
+
+function promptMenu() {
+    inquirer.prompt(otherEmployeesQuestions).then(answers => {
+        console.log(answers);
+        if (answers.otherEmployees === "Engineer") {
+            inquirer.prompt(engineerQuestions).then(answers => {
+                console.log(answers)
+            })
+            if (answers.otherEmployees === "Intern") {
+                inquirer.prompt(internQuestions).then(answers => {
+                    console.log(answers)
+                })
+            }
+        }
+    })
+}
 
 function promptEngineer() {
     promptMenu();
