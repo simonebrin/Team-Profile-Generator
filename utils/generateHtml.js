7606414c845f754bf8ec40c34a62ff35c84854e9
Simone Brin <simonebrin@gmail.com>
2022-08-29 11:55:46 -0400

    added more html fixed how some functions were showing up

--- utils/generateHtml.js
+++ utils/generateHtml.js
@@ -11,23 +11,34 @@ return`
     <title>Team Profile Generator</title>
 </head>
 <body>
-    <h1>Your Team</h1>
+    <div class="container-fluid text-bg-danger p-3 text-center">
+        <h1>My Team</h1>
+    </div>
     ${team.map(teamMember => {
         let specialAttribute
         if(teamMember.getRole() === "Manager") specialAttribute = teamMember.getOffice()
         if(teamMember.getRole() === "Engineer") specialAttribute = teamMember.getGithub()
         if(teamMember.getRole() === "Intern") specialAttribute = teamMember.getSchool()
         return`
-        <div class="member-card"
-        <h3>${teamMember.getRole()}</h3>
-        ${teamMember.getName()}
-        ${teamMember.getEmail()}
-        ${teamMember.getId()}
-        ${specialAttribute}
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+        <div class="col">
+          <div class="card h-100">
+        <div class="card-body text-bg-primary text-center">
+          <h4 class="card-title">${teamMember.getName()}</h4>
+          <h5 class="card-text">${teamMember.getRole()}</h5>
+        </div>
+        <ul class="list-group list-group-flush text-center">
+          <li class="list-group-item">${teamMember.getEmail()}</li>
+          <li class="list-group-item">${teamMember.getId()}</li>
+          <li class="list-group-item">${specialAttribute}</li>
+        </ul>
+        </div>
         </div>
         `
-    }) .join("")}
-    
+    }) }
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 </html>`
 }
